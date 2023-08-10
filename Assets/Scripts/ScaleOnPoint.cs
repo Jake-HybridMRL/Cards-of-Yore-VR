4d22267043c347dfa3b0eba891f8a6b4288b5fa3
friedSushis <aadit_sengupta@asl.org>
2023-08-10 15:56:31 +0100

    gauntlet in balcony

--- Assets/Scripts/ScaleOnPoint.cs
+++ Assets/Scripts/ScaleOnPoint.cs
@@ -7,6 +7,8 @@ public class ScaleOnPoint : MonoBehaviour
 {
     GameObject Card;
     Vector3 scaleChange = new Vector3(0.4f,0.8f,0.12f);
+    Vector3 initialScale = new Vector3(0.05f,0.1f,0.015f);
+    bool sizedUp = false;
 
 
     // Start is called before the first frame update
@@ -28,16 +30,27 @@ public class ScaleOnPoint : MonoBehaviour
         tran.localScale -= scaleChange;
     }
 
+        private void SizeOriginalCard(Transform tran){
+        tran.localScale = initialScale;
+    }
+
     void OnTriggerEnter(Collider trigCol){
         if(trigCol.GetComponent<CardBehaviour>() != null && trigCol.gameObject.name == "Card" && trigCol.GetComponent<CardBehaviour>().cardInSlot){
             SizeUpCard(trigCol.transform);
+            sizedUp = true;
         }
     }
 
         void OnTriggerExit(Collider trigCol){
         if(trigCol.GetComponent<CardBehaviour>() != null && trigCol.gameObject.name == "Card" && trigCol.GetComponent<CardBehaviour>().cardInSlot){
             SizeDownCard(trigCol.transform);
+            sizedUp = false;
+
         }
+        if(Card.GetComponent<CardBehaviour>() != null && !Card.GetComponent<CardBehaviour>().cardInSlot){
+            SizeOriginalCard(trigCol.transform);
+        }
+
     }
 
     // Update is called once per frame
